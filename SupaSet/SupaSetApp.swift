20e5740d96b12a10e4a4d514a83d4f2d26654656
Rishi Garg <gargrishi02@gmail.com>
2025-06-25 22:27:18 +0530

    Initial Commit

---
+++ SupaSet/SupaSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupaSetApp.swift
+//  SupaSet
+//
+//  Created by Rishi Garg on 6/25/25.
+//
+
+import SwiftUI
+
+@main
+struct SupaSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
